1b488878e77dcf848823667a85c11983846ca064
Istvan <steven.ford.j@gmail.com>
2021-07-08 18:05:38 +0300

    Solved the input, randoming the team members but still not teams

---
+++ js.js
@@ -0,0 +1,98 @@
+// Buttons
+
+const addMem = document.getElementById('addMem')
+const clearMem = document.getElementById('clearMem')
+
+const randomMem = document.getElementById('randomMem')
+
+// Inputs random
+const memNum = document.getElementById('memb-num')
+const teamNum = document.getElementById('team-num')
+
+// Data base
+let memBase = 0
+let numberM = 0
+let numberT = 0
+
+// outputs
+const memBaseOut = document.getElementById('members')
+
+
+// listeners
+addMem.addEventListener('click', addThisMem)
+clearMem.addEventListener('click', clearThisMem)
+
+randomMem.addEventListener('click', randomThisMem)
+
+// Fucntions
+
+// adding people
+function addThisMem(){
+    let member = document.getElementById('membersID').value
+    memBase = member.split(/(?!\(.*)\s(?![^(]*?\))/g)
+    for( i = 0; i< memBase.length; i++){
+        let par = document.createElement('p')
+        par.innerText = memBase[i]
+        memBaseOut.appendChild(par)
+    }
+    let clearInner = document.getElementById('membersID')
+    clearInner.value = ''
+}
+
+// clearing people
+function clearThisMem(){
+    memBase = 0
+    let names = document.querySelectorAll('#members p')
+    for (let name of names){
+        name.remove()
+    }
+}
+
+
+// random var
+let x = 0
+let y =[]
+
+// randoming in teams
+function randomThisMem(){
+    numberM = memNum.value
+    randomaizer(memBase.length)
+    numberT = teamNum.value
+    let tempBase = []
+
+    for(i=0; i<numberT;i++){ //Making teams
+    // making members of team
+        for(i=0; i<numberM; i++){
+            randomaizer(memBase.length)
+            while (y.includes(x) !== false){
+                randomaizer(memBase.length)
+            }
+            y.push(x)
+            tempBase.push(memBase[x])
+        }
+        
+        // teams
+        let div = document.createElement('div')
+        div.classList.add('col')
+        div.appendChild(memBaseOut)
+            for(j=0; j<tempBase.length; i++){
+                let parr = document.createElement('p')
+                parr.innerText = tempBase[j]
+            div.appendChild(parr)
+            }
+    }
+
+    tempBase = []
+    console.log(tempBase)
+}
+
+function randomTeam (){
+    numberT = teamNum.value
+    for(i=0; i<numberT;i++){
+        
+    }
+}
+
+function randomaizer(b){
+    x = Math.round(Math.random()*(b-0)+0)
+}
